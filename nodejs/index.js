88d25569363159883e1f25afaf0e3b20148210df
trongthongvo <56016681+trongthongvo@users.noreply.github.com>
2020-11-02 10:47:40 +0700

    Add files via upload

---
+++ nodejs/index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const app_env = process.env.APP_ENV || "unknown";
+const app_version = process.env.APP_VERSION || "unknown";
+const app_host = process.env.APP_HOST || "unknown";
+
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    //const msg = 'Hello Node!\n'
+    const msg = `Hello Word! Application version ${app_version} - Environment ${app_env} - ${app_host}\n`
+    res.end(msg);
+});
+
+server.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}/`);
+});
